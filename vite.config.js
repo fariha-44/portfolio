a5f0a53e5eb7dd01208c22e664241c4641bf8b38
farhia <farxiyafarah770@gmail.com>
2025-10-25 19:33:08 +0800

    update homepage and vite base for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // <--- ✅ this must match your repo name
+  base: '/portfolio/', 
 })
